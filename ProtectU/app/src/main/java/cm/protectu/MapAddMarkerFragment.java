b6c830c2d880eafad195b326f31c7d22a692b3ad
henriqueleote <80213140+henriqueleote@users.noreply.github.com>
2022-04-26 02:00:06 +0100

    Changed the map loading animation

--- ProtectU/app/src/main/java/cm/protectu/MapAddMarkerFragment.java
+++ ProtectU/app/src/main/java/cm/protectu/MapAddMarkerFragment.java
@@ -196,8 +196,9 @@ public class MapAddMarkerFragment extends Fragment {
                             gMap = googleMap;
                             //TODO CHECK IF IT ROTATES WHEN ON MOBILE
                             googleMap.setMyLocationEnabled(true);
-                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+
+                            //Loads the map without animation with the device's current location in the map
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 12));
 
                             gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                                 @Override
